097b7613e590bddd750d150b68b6b2a447bceefd
win rf.wei <295238640@qq.com>
2018-06-17 19:04:22 +0800

    ef

---
+++ DAL/DbConnect.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DbConnect : db_booxl_tsl_acEntities
+    {
+        public DbConnect()
+        {
+            this.Database.Log = log;
+        }
+        private void log(string par)
+        {
+            Console.WriteLine(par);
+        }
+
+        private static DbConnect dbConnect = null;
+        //public static DbConnect Instance()
+        //{
+        //    if (dbConnect ==null)
+        //    {
+        //        dbConnect = new DbConnect();
+        //    }
+        //    return dbConnect;
+        //}
+
+        public static DbConnect Instance
+        {
+            get
+            {
+                if (dbConnect == null) { dbConnect = new DbConnect(); }
+                return dbConnect;
+            }
+        }
+    }
+}
---
+++ DAL/Tool/Convert.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Tool
+{
+    public class Convert
+    {
+        public static void To<T,Y>(T obj,ref Y res)
+        {
+            Type t_res = res.GetType();
+            var p_res = t_res.GetProperties();
+
+            Type from_type = obj.GetType();
+            foreach(var ff in p_res)
+            {
+                var temp = from_type.GetProperty(ff.Name);
+                if (temp != null)
+                {
+                    ff.SetValue(res, temp.GetValue(obj));
+                }
+            }
+        }
+
+        public static T To<T>(object from)
+        {
+            //Type res_type = typeof(T);
+            T res = Activator.CreateInstance<T>();
+            To(from, ref res);
+            return res;
+        }
+
+    }
+}
